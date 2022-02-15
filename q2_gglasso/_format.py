c5b37cbf2bf5ac36f801a51db41c23e6d2548339
Vlasovets <otorrent@mail.ru>
2022-02-15 22:37:03 +0100

    add directory format for gglasso

---
+++ q2_gglasso/_format.py
@@ -0,0 +1,25 @@
+import qiime2.plugin.model as model
+from qiime2.plugin import ValidationError
+import pandas as pd
+from itertools import combinations
+import zarr
+import numpy as np
+
+
+class GGLassoDataFormat(model.TextFileFormat):
+    def validate(self, level):
+        pass
+
+
+PairwiseFeatureDataDirectoryFormat = model.SingleFileDirectoryFormat(
+    'PairwiseFeatureDataDirectoryFormat', 'pairwise_comparisons.tsv', GGLassoDataFormat)
+
+
+
+# class ZarrProblemFormat(model.BinaryFileFormat):
+#     def validate(self, level):
+#         pass
+#
+#
+# CLASSOProblemDirectoryFormat = model.SingleFileDirectoryFormat('CLASSOProblemDirectoryFormat',
+#                                                                'problem.zip', format=ZarrProblemFormat)
