21d5731e377d90318d1cab8ed363a59ae1cf0ec9
Vlasovets <otorrent@mail.ru>
2022-02-15 22:38:12 +0100

    calculate covariance and spearman correlation

--- q2_gglasso/_func.py
+++ q2_gglasso/_func.py
@@ -3,7 +3,7 @@ import numpy as np
 from biom.table import Table
 import pandas as pd
 import zarr
-from scipy.stats import spearmanr
+from scipy import stats
 import multiprocessing
 from functools import partial
 import warnings
@@ -15,75 +15,10 @@ from gglasso.problem import glasso_problem
 from gglasso.helper.data_generation import generate_precision_matrix, group_power_network, sample_covariance_matrix
 from gglasso.helper.utils import log_transform, zero_replacement
 
-
 from q2_types.feature_table import FeatureTable, Composition
 from q2_types.feature_data import FeatureData
 
 import pandas as pd
-
-
-def transform_features(
-    table: pd.DataFrame, transformation: str = "clr", pseudocount: float = 0.5
-) -> pd.DataFrame:
-    if transformation == "clr":
-        X = zero_replacement(table, c=pseudocount)
-        X = log_transform(X)
-
-        return pd.DataFrame(
-            data=X, index=list(table.index), columns=list(table.columns)
-        )
-
-    else:
-        raise ValueError(
-            "Unknown transformation name, use clr and not %r" % transformation
-        )
-
-
-def pairwise_iter_wo_metadata(pairwise_iter):
-    for (val_i, id_i, _), (val_j, id_j, _) in pairwise_iter:
-        yield ((val_i, id_i), (val_j, id_j))
-
-def calculate_correlation(data, corr_method=spearmanr):
-    (val_i, id_i), (val_j, id_j) = data
-    r, p = corr_method(val_i, val_j)
-    return (id_i, id_j), (r, p)
-
-
-def calculate_correlations(table: Table, corr_method=spearmanr, nprocs=1) -> pd.DataFrame:
-    if nprocs > multiprocessing.cpu_count():
-        warnings.warn("nprocs greater than CPU count, using all avaliable CPUs")
-        nprocs = multiprocessing.cpu_count()
-
-    pool = multiprocessing.Pool(nprocs)
-    cor = partial(calculate_correlation, corr_method=corr_method)
-    results = pool.map(cor, pairwise_iter_wo_metadata(table.iter_pairwise(axis='observation')))
-    index = [i[0] for i in results]
-    data = [i[1] for i in results]
-    pool.close()
-    pool.join()
-    correls = pd.DataFrame(data, index=index, columns=['r', 'p'])
-    # Turn tuple index into actual multiindex, now guaranteeing that correls index is sorted
-    correls.index = pd.MultiIndex.from_tuples([sorted(i) for i in correls.index])
-
-    return correls
-
-
-def calculate_covariance(table: Table,
-                         method: str,
-                         bias: bool = True,
-                         ) -> pd.DataFrame:
-
-    if method == "unscaled":
-
-        print("Calculate {0} covariance matrices S".format(method))
-        S = np.cov(table, bias=bias)
-
-    # elif method is "correlation":
-    #     print("Calculate Spearman's {0} matrices C".format(method))
-    #     C = spearmanr(table)
-    else:
-        raise ValueError('Provided covariance calculation method is not supported.')
-    return S
 
 
 def to_zarr(obj, name, root, first=True):
@@ -120,3 +55,38 @@ def to_zarr(obj, name, root, first=True):
     else:
         to_zarr(obj.__dict__, name, root, first=first)
 
+
+def transform_features(
+        table: pd.DataFrame, transformation: str = "clr", pseudocount: float = 0.5
+) -> pd.DataFrame:
+    if transformation == "clr":
+        X = zero_replacement(table, c=pseudocount)
+        X = log_transform(X)
+
+        return pd.DataFrame(
+            data=X, index=list(table.index), columns=list(table.columns)
+        )
+
+    else:
+        raise ValueError(
+            "Unknown transformation name, use clr and not %r" % transformation
+        )
+
+
+def calculate_covariance(table: pd.DataFrame,
+                         method: str,
+                         bias: bool = True,
+                         ) -> pd.DataFrame:
+    # X = table.to_dataframe()
+
+    if method == "unscaled":
+        print("Calculate {0} covariance matrices S".format(method))
+        result = np.cov(table, bias=bias)
+
+    elif method == "spearmanr":
+        result, pval = stats.spearmanr(table, table)
+
+    else:
+        raise ValueError('Given covariance calculation method is not supported.')
+
+    return pd.DataFrame(result)
