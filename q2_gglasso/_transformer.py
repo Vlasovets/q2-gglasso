761f4099d6b53fc5851edd8620c020a47ff8971a
Vlasovets <otorrent@mail.ru>
2022-03-15 21:00:56 +0100

    reading gglasso format to pandas

--- q2_gglasso/_transformer.py
+++ q2_gglasso/_transformer.py
@@ -3,17 +3,28 @@ import qiime2
 
 from q2_gglasso.plugin_setup import plugin
 from q2_gglasso._format import GGLassoDataFormat
+from biom import load_table
+
 
 @plugin.register_transformer
 def _1(data: pd.DataFrame) -> GGLassoDataFormat:
     ff = GGLassoDataFormat()
     with ff.open() as fh:
-        data.to_csv(fh, sep='\t', index_label=('feature1', 'feature2'))
+        # data.to_csv(fh, sep='\t', index_label=('feature1', 'feature2'))
+        data.to_csv(fh, sep='\t', index_label='feature1')
     return ff
+
 
 @plugin.register_transformer
 def _2(ff: GGLassoDataFormat) -> pd.DataFrame:
-    df = pd.read_table(str(ff), index_col=(0, 1), sep='\t')
-    new_index = pd.MultiIndex.from_tuples([(str(i), str(j)) for i, j in df.index])
-    df.index = new_index
+    table = load_table(str(ff))
+    df = table.to_dataframe()
     return df
+
+# @plugin.register_transformer
+# def _2(ff: GGLassoDataFormat) -> pd.DataFrame:
+#     df = pd.read_csv(str(ff), index_col=0, sep='\t')
+#     df = df.dropna(axis=1, how='all')
+#     new_index = pd.MultiIndex.from_tuples([(str(i), str(j)) for i, j in df.index])
+#     df.index = new_index
+#     return df
