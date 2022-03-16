bf8e359ba49134b2bb8febf6b1ab09477bbe0bf3
Vlasovets <otorrent@mail.ru>
2022-03-16 23:20:52 +0100

    cleaning clr-transform function

--- q2_gglasso/_func.py
+++ q2_gglasso/_func.py
@@ -13,7 +13,8 @@ from q2_types.feature_data import FeatureData
 
 from gglasso.problem import glasso_problem
 from gglasso.helper.data_generation import generate_precision_matrix, group_power_network, sample_covariance_matrix
-from gglasso.helper.utils import log_transform, zero_replacement
+from gglasso.helper.utils import log_transform, normalize
+from gglasso.helper.basic_linalg import scale_array_by_diagonal
 from gglasso.helper.model_selection import aic, ebic, K_single_grid
 
 from q2_types.feature_table import FeatureTable, Composition
@@ -57,15 +58,15 @@ def to_zarr(obj, name, root, first=True):
 
 
 def transform_features(
-        table: pd.DataFrame, transformation: str = "clr", pseudocount: float = 0.5
+        table: pd.DataFrame, transformation: str = "clr",
 ) -> pd.DataFrame:
+
     if transformation == "clr":
-        X = zero_replacement(table, c=pseudocount)
+
+        X = normalize(table)
         X = log_transform(X)
 
-        return pd.DataFrame(
-            data=X, index=list(table.index), columns=list(table.columns)
-        )
+        return pd.DataFrame(X)
 
     else:
         raise ValueError(
@@ -77,14 +78,16 @@ def calculate_covariance(table: pd.DataFrame,
                          method: str,
                          bias: bool = True,
                          ) -> pd.DataFrame:
-    # X = table.to_dataframe()
+
+    S = np.cov(table.values, bias=bias)
 
     if method == "unscaled":
         print("Calculate {0} covariance matrices S".format(method))
-        result = np.cov(table, bias=bias)
+        result = S
 
-    elif method == "spearmanr":
-        result, pval = stats.spearmanr(table, table)  # TODO Scaling function here
+    elif method == "scaled":
+        print("Calculate {0} covariance (correlation) matrices S".format(method))
+        result = scale_array_by_diagonal(S)
 
     else:
         raise ValueError('Given covariance calculation method is not supported.')
@@ -92,30 +95,7 @@ def calculate_covariance(table: pd.DataFrame,
     return pd.DataFrame(result)
 
 
-
-
-
-# def solve_problem(covariance_matrix: pd.DataFrame,
-#                   lambda1: float = .05) -> pd.DataFrame:
-#
-#     S = covariance_matrix.values
-#
-#     P = glasso_problem(S, N=1, reg_params={'lambda1': lambda1}, latent=False, do_scaling=False)
-#     P.solve()
-#     sol = P.solution.precision_
-#
-#     # if modelselect_params:
-#     #
-#     #     P.model_selection(modelselect_params=modelselect_params, method=method, gamma=gamma)
-#
-#     # else:
-#     #     P.solve()
-#     #     sol = P.solution.precision_
-#
-#     return pd.DataFrame(sol)
-
-
-def solve_problem(covariance_matrix: pd.DataFrame, lambda1: float=0.05) -> pd.DataFrame:
+def solve_problem(covariance_matrix: pd.DataFrame, lambda1: float = 0.05) -> pd.DataFrame:
 
     S = covariance_matrix.values
 
@@ -124,34 +104,3 @@ def solve_problem(covariance_matrix: pd.DataFrame, lambda1: float=0.05) -> pd.Da
     sol = P.solution.precision_
 
     return pd.DataFrame(sol)
-
-# def solve_problem(covariance_matrix: pd.DataFrame,
-#                  problem: str,
-#                  lambda1: list,
-#                  n_samples: int,
-#                  method: str = 'eBIC',
-#                  gamma: float = 0.3,
-#                  latent: bool = False,
-#                  use_block: bool = True
-#                  ) -> pd.DataFrame:
-#
-#     if problem == "single":
-#         print("Solve {0} Graphical Lasso problem".format(method))
-#
-#         covariance_matrix = np.array(covariance_matrix)
-#         covariance_matrix = covariance_matrix.reshape(1, covariance_matrix.shape[0], covariance_matrix.shape[1])
-#
-#         est_uniform, est_indv, statistics = K_single_grid(covariance_matrix,
-#                                                           lambda_range=lambda1,
-#                                                           N=n_samples,
-#                                                           method=method,
-#                                                           gamma=gamma,
-#                                                           latent=latent,
-#                                                           use_block=use_block)
-#
-#         result = est_uniform["Theta"].reshape(covariance_matrix.shape[0], covariance_matrix.shape[1])
-#
-#     else:
-#         raise ValueError('Given Graphical lasso problem is not supported.')
-#
-#     return pd.DataFrame(result)
