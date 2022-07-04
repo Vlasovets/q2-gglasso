1e9da0fa6b27b01dccabd1441eb77f1d67dec5ae
Vlasovets <otorrent@mail.ru>
2022-07-04 13:11:25 +0200

    test zarr format

---
+++ q2_gglasso/tests/test_zarr.py
@@ -0,0 +1,53 @@
+import unittest
+import zarr
+import q2_gglasso as q2g
+import pandas as pd
+import numpy as np
+from gglasso.problem import glasso_problem
+
+try:
+    from q2_gglasso._func import solve_problem
+
+except ImportError:
+    raise ImportWarning('Qiime2 not installed.')
+
+
+class TestUtil(unittest.TestCase):
+    def test_zarr_format(self):
+        table = pd.DataFrame([[1, 1, 7, 3],
+                              [2, 6, 2, 4],
+                              [5, 5, 3, 3],
+                              [3, 2, 8, 1]],
+                             index=['s1', 's2', 's3', 's4'],
+                             columns=['o1', 'o2', 'o3', 'o4'])
+
+        S = np.cov(table.values)
+        n_samples = table.shape[0]
+        reg_params = {'lambda1': [0.5, 0.01], "mu1": [0.5, 0.1]}
+
+        P = glasso_problem(S, N=n_samples, latent=True)
+        P.model_selection(modelselect_params=reg_params)
+
+        # save GGLasso solution
+        solution = P.__dict__['solution']
+
+        # write GGLasso solution to zarr format
+        zipfile = str("problem.zip")
+        store = zarr.ZipStore(zipfile, mode="w")
+        root = zarr.open(store=store)
+        q2g.to_zarr(P.__dict__, "problem", root)
+        store.close()
+
+        # read GGLasso solution from zarr format
+        store_new = zarr.ZipStore(str("problem.zip"), mode="r")
+        root_new = zarr.open(store=store_new)
+
+        # check if these solutions are equal
+        for key, value in solution.__dict__.items():
+            if "solution" in root_new:
+                if key in root_new["solution/"]:
+                    zarr_value = np.array(root_new["solution/" + key])
+                    try:
+                        value == zarr_value
+                    except:
+                        self.assertNotEqual(value, zarr_value)
