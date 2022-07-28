5efc9670ee175db73a248b1e557f8a7d6177c78a
Vlasovets <otorrent@mail.ru>
2022-07-28 18:25:59 +0200

    add non-conforming case

--- q2_gglasso/_func.py
+++ q2_gglasso/_func.py
@@ -17,13 +17,14 @@ from q2_types.feature_data import FeatureData
 from gglasso.problem import glasso_problem
 from gglasso.helper.data_generation import generate_precision_matrix, group_power_network, sample_covariance_matrix
 from gglasso.helper.basic_linalg import scale_array_by_diagonal
+from gglasso.helper.ext_admm_helper import create_group_array, construct_indexer, check_G
 from gglasso.helper.model_selection import aic, ebic, K_single_grid
 
 from q2_types.feature_table import FeatureTable, Composition
 from q2_types.feature_data import FeatureData
 
-from .utils import if_2d_array, if_model_selection, if_all_none, list_to_array, if_non_conforming
-from .utils import normalize, log_transform, to_zarr
+from .utils import if_2d_array, if_model_selection, if_all_none, list_to_array
+from .utils import normalize, log_transform, to_zarr, transpose_dataframes
 
 
 def transform_features(
@@ -49,6 +50,38 @@ def transform_features(
         )
 
 
+def build_groups(tables: Table, check_groups: bool = True) -> np.ndarray:
+
+    columns_dict = dict()
+    dataframes = list()
+    i = 0
+    for table in tables:
+        df = table.to_dataframe()
+        dataframes.append(df)
+        columns_dict[i] = df.columns
+        i += 1
+
+    for k in range(0, len(columns_dict) - 1):
+        non_conforming_problem = set(columns_dict[k].difference(columns_dict[k + 1]))
+        if non_conforming_problem:
+            tables_trans = transpose_dataframes(dataframes)
+            # we transpose dataframes, so the shape of each dataframe becomes (n_variables, n_samples)
+            ix_exist, ix_location = construct_indexer(list(tables_trans))
+
+            G = create_group_array(ix_exist, ix_location)
+
+            # print("Dimensions p_k: ", p_arr)
+            # print("Sample sizes N_k: ", num_samples)
+
+            print("Number of groups found: ", G.shape[1])
+
+            return G
+
+        else:
+            print("All datasets have exactly the same number of features.")
+
+
+
 def calculate_covariance(table: pd.DataFrame,
                          method: str,
                          bias: bool = True,
@@ -68,25 +101,6 @@ def calculate_covariance(table: pd.DataFrame,
         raise ValueError('Given covariance calculation method is not supported.')
 
     return pd.DataFrame(result)
-
-
-def solve_non_conforming(S: np.ndarray, N: list, G: np.ndarray, latent: bool = None, model_selection: bool = None,
-                         reg: str = 'GGL', lambda1: list = None, lambda2: list = None, mu1: list = None):
-
-    if model_selection:
-        print("\tDD MODEL SELECTION:")
-        modelselect_params = {'lambda1_range': lambda1, 'lambda2_range': lambda2, 'mu1_range': mu1}
-        P = glasso_problem(S, N=N, G=G, latent=latent, reg=reg)
-        P.model_selection(modelselect_params=modelselect_params)
-        print(P.__dict__["modelselect_params"])
-
-    else:
-        print("\tWITH LAMBDA1={0}, LAMBDA2={1} and MU={2}".format(lambda1, lambda2, mu1))
-        P = glasso_problem(S, N=N, G=G, reg_params={'lambda1': lambda1, 'lambda2': lambda2, "mu1": mu1},
-                           latent=latent, reg=reg)
-        P.solve()
-
-    return P
 
 
 def solve_SGL(S: np.ndarray, N: list, latent: bool = None, model_selection: bool = None,
@@ -124,13 +138,30 @@ def solve_MGL(S: np.ndarray, N: list, reg: str, latent: bool = None, model_selec
     return P
 
 
+def solve_non_conforming(S: np.ndarray, N: list, G: list, latent: bool = None, model_selection: bool = None,
+                         lambda1: list = None, lambda2: list = None, mu1: list = None):
 
-def solve_problem(covariance_matrix: list, n_samples: list, latent: bool = None,
-                  lambda1: list = None, lambda2: list = None, mu1: list = None, reg: str = 'GGL') \
+    if model_selection:
+        print("\tDD MODEL SELECTION:")
+        modelselect_params = {'lambda1_range': lambda1, 'lambda2_range': lambda2, 'mu1_range': mu1}
+        P = glasso_problem(S, N=N, G=G, latent=latent, reg='GGL')
+        P.model_selection(modelselect_params=modelselect_params)
+        print(P.__dict__["modelselect_params"])
+
+    else:
+        print("\tWITH LAMBDA1={0}, LAMBDA2={1} and MU={2}".format(lambda1, lambda2, mu1))
+        P = glasso_problem(S, N=N, G=G, reg_params={'lambda1': lambda1, 'lambda2': lambda2, "mu1": mu1},
+                           latent=latent, reg='GGL')
+        P.solve()
+
+    return P
+
+
+def solve_problem(covariance_matrix: list, n_samples: list, latent: bool = None, non_conforming: bool = None,
+                  lambda1: list = None, lambda2: list = None, mu1: list = None,  G: list = None, reg: str = 'GGL') \
         -> glasso_problem:
     S = np.array(covariance_matrix)
     S = if_2d_array(S)
-    #G = if_non_conforming(S)
 
     n_samples = list_to_array(n_samples)
 
@@ -157,17 +188,30 @@ def solve_problem(covariance_matrix: list, n_samples: list, latent: bool = None,
     # if 3d array => solve MGL
     elif S.ndim == 3:
 
-        if latent:
-            print("\n----SOLVING {0} PROBLEM WITH LATENT VARIABLES-----".format(reg))
+        if non_conforming:
 
-            P = solve_MGL(S=S, N=n_samples, reg=reg, latent=latent, model_selection=model_selection,
-                          lambda1=lambda1, lambda2=lambda2, mu1=mu1)
+            if latent:
+                print("\n----SOLVING NON-CONFORMING PROBLEM WITH LATENT VARIABLES-----")
+
+                P = solve_non_conforming(S=S, N=n_samples, G=G, latent=latent, model_selection=model_selection,
+                                         lambda1=lambda1, lambda2=lambda2, mu1=mu1)
+            else:
+                print("\n----SOLVING NON-CONFORMING PROBLEM-----")
+
+                P = solve_non_conforming(S=S, N=n_samples, G=G, latent=latent, model_selection=model_selection,
+                                         lambda1=lambda1, lambda2=lambda2, mu1=mu1)
 
         else:
-            print("\n----SOLVING {0} PROBLEM-----".format(reg))
+            if latent:
+                print("\n----SOLVING {0} PROBLEM WITH LATENT VARIABLES-----".format(reg))
 
-            P = solve_MGL(S=S, N=n_samples, reg=reg, latent=latent, model_selection=model_selection,
-                          lambda1=lambda1, lambda2=lambda2, mu1=mu1)
+                P = solve_MGL(S=S, N=n_samples, reg=reg, latent=latent, model_selection=model_selection,
+                              lambda1=lambda1, lambda2=lambda2, mu1=mu1)
+            else:
+                print("\n----SOLVING {0} PROBLEM-----".format(reg))
+
+                P = solve_MGL(S=S, N=n_samples, reg=reg, latent=latent, model_selection=model_selection,
+                              lambda1=lambda1, lambda2=lambda2, mu1=mu1)
 
     return P
 
