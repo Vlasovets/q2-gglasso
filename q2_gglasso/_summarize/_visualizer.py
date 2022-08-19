4dcb74361935ac47bc90bc49544b48d7d5c1d63f
Vlasovets <otorrent@mail.ru>
2022-08-19 11:49:43 +0200

    update summary

--- q2_gglasso/_summarize/_visualizer.py
+++ q2_gglasso/_summarize/_visualizer.py
@@ -1,15 +1,17 @@
 import os
 import pkg_resources
+import qiime2
 import numpy as np
 import zarr
 import pandas as pd
 import q2templates
 from q2_gglasso.utils import flatten_array
 
-TEMPLATES = pkg_resources.resource_filename('q2_gglasso._summarize', '_summarize')
+TEMPLATES = pkg_resources.resource_filename('q2_gglasso', '_summarize')
 
 
-def summarize(output_dir, solution: zarr.hierarchy.Group) -> None:
+def summarize(output_dir, solution: zarr.hierarchy.Group,
+              sample_metadata: qiime2.Metadata = None) -> None:
 
     sparsity = flatten_array(solution['modelselect_stats/SP'])
     lambda_path = flatten_array(solution['modelselect_stats/LAMBDA'])
@@ -21,9 +23,13 @@ def summarize(output_dir, solution: zarr.hierarchy.Group) -> None:
 
     stats_df = pd.DataFrame.from_dict(stats)
 
+    stats_df.to_csv(os.path.join(output_dir, 'stats.csv'))
+
     stats_table = q2templates.df_to_html(stats_df)
+    context = {'stats_table': stats_table}
 
-    context = {'feature_frequencies_table': stats_table}
+    stats_table_template = os.path.join(TEMPLATES, 'assets', 'index.html')
+    index = os.path.join(TEMPLATES, 'assets', 'index.html')
 
-    index_fp = os.path.join(TEMPLATES, 'index.html')
-    q2templates.render(index_fp, output_dir, context=context)
+    templates = [index, stats_table_template]
+    q2templates.render(templates, output_dir, context=context)
