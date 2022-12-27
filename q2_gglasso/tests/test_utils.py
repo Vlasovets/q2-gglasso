a1a3264a6bce9920d5ff35202f7e3cf7e8cf6ff3
Vlasovets <otorrent@mail.ru>
2022-12-27 14:55:33 +0100

    test utils

--- q2_gglasso/tests/test_utils.py
+++ q2_gglasso/tests/test_utils.py
@@ -10,29 +10,16 @@ try:
 except ImportError:
     raise ImportWarning('Qiime2 not installed.')
 
-class TestUtil(unittest.TestCase):
+# class TestUtil(unittest.TestCase):
+#
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+#
 
-    def test_assertWarns(self):
-        with self.assertWarnsRegex(Warning, r'lambda is on the edge of the interval'):
-            p = 32
-            N = 10
-            K = 3
-
-            Sigma, Theta = generate_precision_matrix(p=p, M=1, style='erdos', prob=0.1, seed=1234)
-            S, sample = sample_covariance_matrix(Sigma, N)
-
-            S_SGL = S
-
-            lambda1 = 0.05
-            P_org = glasso_problem(S=S_SGL, reg_params={'lambda1': lambda1}, N=N, latent=False)
-            P_org.solve()
-            #print("hi")
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-
-### test heatmap
+# def test_assertWarns(self):
+#     with self.assertWarnsRegex(Warning, 'The solution might have not reached global minimum!'):
+#         warnings.warn("The solution might have not reached global")
 ### test biom editing
 
