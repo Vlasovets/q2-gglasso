479a86efdcadf5deaaa8da363f9a0d48e70adc6f
Vlasovets <otorrent@mail.ru>
2025-05-29 16:58:17 +0200

    Fix typo in test_utils.py unittest import

--- q2_gglasso/tests/test_utils.py
+++ q2_gglasso/tests/test_utils.py
@@ -4,27 +4,18 @@ This module contains tests for various utility functions in the q2-gglasso plugi
 Currently, most tests are commented out and may be implemented in the future.
 """
 
-import unittes
+import unittest
 import warnings
-from gglasso.helper.data_generation import generate_precision_matrix, sample_covariance_matrix
-
-from gglasso.problem import glasso_problem
 
 try:
     from q2_gglasso._func import solve_problem
-
 except ImportError:
     raise ImportWarning('Qiime2 not installed.')
 
-# class TestUtil(unittest.TestCase):
-#
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
-#
+class TestUtil(unittest.TestCase):
+    def test_dummy(self):
+        """Placeholder test to ensure the test infrastructure works."""
+        self.assertTrue(True)
 
-# def test_assertWarns(self):
-#     with self.assertWarnsRegex(Warning, 'The solution might have not reached global minimum!'):
-#         warnings.warn("The solution might have not reached global")
-# test biom editing
+if __name__ == '__main__':
+    unittest.main()
